22f89fe5651929e90f71b05085064014931e9187
jhomjhonata <jhomjhonata@gmail.com>
2020-05-16 17:19:29 -0300

    Ajuste de configs do banner slider

--- 2º Projeto/js/function.js
+++ 2º Projeto/js/function.js
@@ -1,1 +1,10 @@
-$('.slider').slick();
+$('.slider').slick({
+    infinit: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots:true,
+    /*speed:200,*/
+    fade:true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
